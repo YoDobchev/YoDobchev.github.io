b70c9b8bca23458f67ea00315d2f49a0dc7ada7b
YoDobchev <yo.dobchev@gmail.com>
2022-06-26 21:31:45 +0300

    school fighter

---
+++ school-fighter/src/index.js
@@ -0,0 +1,242 @@
+var vuprosi = {
+    "abvg":[
+        {
+            "question":"За белтъците са в сила следните твърдения: <br>1. първичната им структура е генетично програмирана <br>2. първичната им структура определя всички по-висши равнища на структурна организация на молекулата <br>3. изградени са от полинуклеотидни вериги<br>4. всички имат четвъртична структура <br>",
+            "answer":"Б) 1 и 2",
+            "options":["А) 1 и 4", "Б) 1 и 2", "В) 1 и 3","Г) 1, 2 и 4"],
+        },
+        {
+            "question":"Кое от изброените органични съединения НЕ влиза в състава на ДНК?",
+            "answer":"А) урацил",
+            "options":["А) урацил","Б) аденин","В) тимин","Г) дезоксирибоза"]
+        },
+        {
+            "question":"ДНК има в:<br>1) рибозомите<br>2) ядрото<br>3) митохондриите<br>4) хлоропластите<br>",
+            "answer":"В) 2, 3 и 4",
+            "options":["А) 1 и 2","Б) 1 и 4","В) 2, 3 и 4","Г) 1, 2 и 3 "]
+        },
+        {
+            "question":"Ако в една молекула ДНК има 24% нуклеотиди с аденин, то колко са нуклеотидите с азотната база гуанин в молекулата?",
+            "answer":"В) 26%",
+            "options":["А) 76%","Б) 52%","В) 26%","Г) 24% "]
+        },
+        {
+            "question":"Как се наричат най-просто устроените клетки, при които генетичният материал е представен от една пръстеновидна молекула ДНК?",
+            "answer":"А) прокариотни",
+            "options":["А) прокариотни","Б) еукариотни","В) полови","Г) соматични"]
+        },
+        {
+            "question":"Кои от следните структурни особености отличават животинската от растителната клетка:<br>1. отсъствие на клетъчна стена<br>2. наличие на комплекс на Голджи<br>3. отсъствие на хлопопласти<br>4. наличие на полизоми",
+            "answer":"В) 1 и 3",
+            "options":["А) 1 и 4", "Б) 2 и 3", "В) 1 и 3", "Г) 1, 2 и 3"]
+        },
+        {
+            "question":"Много бозайници понижават телесната си температура чрез изпотяване. Това се дължи на свойството на водата да:",
+            "answer":"Г) поглъща топлина при разрушаването на водородни връзки",
+            "options":["А) променя плътността си, когато се изпарява", "Б) разтваря молекули във въздуха", "В) отделя топлина при образуването на водородни връзки", "Г) поглъща топлина при разрушаването на водородни връзки"]
+        },
+        {
+            "question":"С термина гликолиза означаваме процесите на:",
+            "answer":"А) разграждане на въглехидрати в цитоплазмата",
+            "options":["А) разграждане на въглехидрати в цитоплазмата", "Б) разграждане на въглехидрати в митохондриите", "В) синтез на въглехидрати в клетката", "Г) поддържане на постоянно количество глюкоза в кръвта"]
+        },
+        {
+            "question":"За жизнения цикъл на крайно специализирани клетки е вярно, че:",
+            "answer":"Г) липсва митотичен цикъл",
+            "options":["А) завършва с митоза", "Б) деленето им се предхожда от автосинтетична интерфаза", "В) е равен по продължителност с митотичния цикъл", "Г) липсва митотичен цикъл"]
+        },
+        {
+            "question":"Амитозата е делене, характерно за:",
+            "answer":"Б) някои протисти",
+            "options":["A) някои неврони и невроглиални клетки", "Б) някои протисти", "В) цианобактерии и дрожди", "Г) мускулни и чернодробни клетки"]
+        }
+    ],
+    "ez":[
+        {
+            "question":"Вирусите са най-малките организми на Земята и са неклетъчни форми на живот.",
+            "answer":false,
+        },
+        {
+            "question":"Тилакоидните мембрани, в които се осъществяват фотосинтетични процеси, са структурни части на хлоропластите.",
+            "answer":true
+        },
+        {
+            "question":"Активният транспорт през клетъчната мембрана се извършва без разход на енергия от клетката.",
+            "answer":false
+        },
+        {
+            "question":"Ендоплазмената мрежа е органел, характерен за еукариотните клетки.",
+            "answer":true
+        },
+        {
+            "question":"Митохондриите, хлоропластите, хромопластите и левкопластите НЕ са двумембранни клетъчни органиели.",
+            "answer":false
+        },
+        {
+            "question":"Лизозомите съдържат смилателни ензими и са универсални органели за всички еукариотни организми.",
+            "answer":true
+        },
+        {
+            "question":"Автотрофно хранене имат организми, които приемат само растителна храна.",
+            "answer":false
+        },
+        {
+            "question":"Транскрипцията, транслацията и репликацията са анаболитни процеси.",
+            "answer":true
+        },
+        {
+            "question":"Транскрипцията е процес, при който се синтезира АТФ за сметка на енергията, отделена при биологочнито окисление в дихателните вериги на митохондриите.",
+            "answer":false
+        },
+        {
+
+            "question":"Вирусите са неклетъчни форми на живот и осъществяват собствени обменни процес и не могат да се разможават самостоятелно.",
+            "answer":true
+        }
+    ],
+    "slojen":[
+        {
+            "question":"На схемата са изписани антикодоните в тРНК1 и тРНК2. Кои са съответстващите им кодони в иРНК, означени на схемата като кодон1 и кодон2?",
+            "image":"../media/30.png"
+        },
+        {
+            "question":"Какво е цитоза, ендоцитоза и екзоцитоза?"
+        },
+        {
+            "question":"Какво е устройството на клетъчното ядро?"
+        },
+        {
+            "question":"Какво представлява транскрипцията?"
+        },
+        {
+            "question":"Какво представлява процеса митоза и как протича?"
+        },
+        {
+            "question":"Какво представлява процеса мейоза и как протича?"
+        }
+    ],
+}
+var currentQuestion
+var questionType
+var turn = true // true - 1; false - 2
+var health1 = 100
+var health2 = 100
+
+function vupros(type){
+    questionType = type;
+    switch(type){
+        case 0:
+            if(vuprosi.ez.length > 0){
+                showDialogueWindow();
+                currentQuestion = Math.floor(Math.random() * vuprosi.ez.length);
+                document.getElementById("question").innerHTML = `<img src="../media/thinking-emoji-30.gif" width="70px" height="70px"></img><br>`;
+                document.getElementById("question").innerHTML += vuprosi.ez[currentQuestion].question;
+
+                document.getElementById("question").innerHTML += `<p class="answer" onclick="answerQuestion(true)"> ДА </p>`;
+                document.getElementById("question").innerHTML += `<p class="answer" onclick="answerQuestion(false)""> НЕ </p>`;
+            }else{
+                alert("Няма повече въпроси от този тип")
+                break;
+            }
+            break;
+        case 1:
+            if(vuprosi.abvg.length > 0){
+                showDialogueWindow();
+                currentQuestion = Math.floor(Math.random() * vuprosi.abvg.length);
+                document.getElementById("question").innerHTML = `<img src="../media/thinking-emoji-30.gif" width="70px" height="70px"></img><br>`;
+                document.getElementById("question").innerHTML += vuprosi.abvg[currentQuestion].question;
+
+                for(var i = 0; i < 4; i++){
+                    document.getElementById("question").innerHTML += `<p id="${i}" class="answer" onclick="answerQuestion(${i})">`;
+                    document.getElementById(`${i}`).innerHTML = vuprosi.abvg[currentQuestion].options[i];
+                }
+            }else{
+                alert("Няма повече въпроси от този тип")
+                break;
+            }
+            break;
+        case 2:
+            if(vuprosi.slojen.length > 0){
+                showDialogueWindow();
+                currentQuestion = Math.floor(Math.random() * vuprosi.slojen.length);
+                document.getElementById("question").innerHTML = `<img src="../media/thinking-emoji-30.gif" width="70px" height="70px"></img><br>`;
+                document.getElementById("question").innerHTML += vuprosi.slojen[currentQuestion].question;
+
+                document.getElementById("question").innerHTML += `<p class="answer" onclick="answerQuestion(true)"> ВЯРНО </p>`;
+                document.getElementById("question").innerHTML += `<p class="answer" onclick="answerQuestion(false)"> ГРЕШНО </p>`;
+
+                if(vuprosi.slojen[currentQuestion].image){
+                    document.getElementById("question").innerHTML += `<img src="${vuprosi.slojen[currentQuestion].image}"></img>`;
+                }
+                
+            }else{
+                 alert("Няма повече въпроси от този тип")
+                break;
+            }
+            break;
+    }
+}
+
+function answerQuestion(answer){
+    switch(questionType){
+        case 0:
+            if(vuprosi.ez[currentQuestion].answer == answer){
+                alert("Вярно")
+                vuprosi.ez.splice(currentQuestion, 1)
+                damage(+(!turn), 5)
+
+            }else{
+                alert("Невярно") 
+            }
+            hideAndSwitch();
+            break;
+        case 1:
+            if(vuprosi.abvg[currentQuestion].options[answer] == vuprosi.abvg[currentQuestion].answer){
+                alert("Вярно")
+                vuprosi.abvg.splice(currentQuestion, 1)
+                damage(+(!turn), 10)
+
+            }else{
+                alert("Невярно") 
+            }
+            hideAndSwitch();
+            break;
+        case 2:
+            if (answer) damage(+(!turn), 15); vuprosi.slojen.splice(currentQuestion, 1);
+            hideAndSwitch();
+            break;
+    }
+}
+
+function damage(team, amount){
+    if(team){
+        if(amount >= health2){
+            alert("Отбор 2 печели")
+            window.location.reload();
+        }
+        health2 -= amount;
+        document.getElementById(`bar${team}`).innerHTML = health2;
+        document.getElementById(`bar${team}`).style.width = `${health2}%`;
+    }else{
+        if(amount >= health1){
+            alert("Отбор 1 печели")
+            window.location.reload();
+        }
+        health1 -= amount;
+        document.getElementById(`bar${team}`).innerHTML = health1;
+        document.getElementById(`bar${team}`).style.width = `${health1}%`;
+    }   
+}
+
+function hideAndSwitch(){
+    document.getElementById("dialogue-window").style.visibility = "hidden";
+    document.getElementById("game").style.filter = "";
+    document.getElementById("tint").style.visibility = "hidden";
+    turn = !turn;
+    document.getElementById("turn").innerHTML = turn ? "На ред: Отбор 1" : "На ред: Отбор 2";
+}
+function showDialogueWindow(){
+    document.getElementById("game").style.filter = "blur(4px)";
+    document.getElementById("tint").style.visibility = "visible";
+    document.getElementById("dialogue-window").style.visibility = "visible";
+}
